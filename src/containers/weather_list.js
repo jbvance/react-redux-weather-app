5b60fa51afa3d173c1152f1130363d87d42b5c6c
Jason Vance <jbvance@gmail.com>
2017-12-27 13:59:09 -0600

    udpate chart component to show average and units of measurement

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart.js'
 
 class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
-        console.log("TEMPS", temps);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
@@ -15,9 +16,14 @@ class WeatherList extends Component {
                     {name}
                 </td>
                 <td>
-                <Sparklines data={temps} width={180} height={120}>
-                    <SparklinesLine color="red" />
-                </Sparklines>
+                    <Chart data={temps} color="orange" units="K" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="blue" units="hPa"/>
+                </td>
+
+                <td>
+                    <Chart data={humidities} color="black" units="%" />
                 </td>
             </tr>
         );
@@ -29,9 +35,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature(K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
