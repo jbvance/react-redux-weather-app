b3b48e305815b51e73cfe260cc571c4426a48ba1
Jason Vance <jbvance@gmail.com>
2017-12-27 12:59:21 -0600

    continue weather reducera and begin weather_list component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
+import axios from 'axios';
 import Config from '../../config';
+
 const API_KEY = Config.api_key;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`
+    // axios get returns a promise
+    const request = axios.get(url);    
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
